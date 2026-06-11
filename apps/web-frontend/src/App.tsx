6cf6694775e43b72716cd3e54aea648cf58ff774
GVirgo <34594713+GVirgo@users.noreply.github.com>
2026-06-11 11:12:36 +0630

    feat: add main App component

---
+++ apps/web-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MovieRecapMobileUI from './components/MovieRecapMobileUI'
+
+function App() {
+  return (
+    <div className="bg-slate-950 min-h-screen">
+      <MovieRecapMobileUI />
+    </div>
+  )
+}
+
+export default App
